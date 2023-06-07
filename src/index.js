fba58552010f550f80758789ee980429c776a874
bharathbonny <36770521+bharathbonny@users.noreply.github.com>
2023-06-08 01:17:32 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Posts, {loader as postsLoader} from './routes/Posts';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import NewPost, {action as newPostAction} from './routes/NewPost';
+import RootLayout from './routes/RootLayout';
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '', 
+    element: <RootLayout />, 
+    children: [
+      {path: '/', 
+      element: <Posts />, 
+      loader: postsLoader,
+      children: [
+          {path: '/create-post', element: <NewPost />, action: newPostAction},
+          {path: '/:id', element: <PostDetails />, loader: postDetailsLoader}
+        ],
+      }, //Home Page
+  ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
